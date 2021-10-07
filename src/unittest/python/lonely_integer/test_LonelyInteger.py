a6973888f83a8185b40a0385bc5c8ab2c4f8caa6
teodor.russu <teodor.russu@bonial.com>
2021-10-07 23:06:04 +0200

    cleanup

--- src/unittest/python/lonely_integer/test_LonelyInteger.py
+++ src/unittest/python/lonely_integer/test_LonelyInteger.py
@@ -5,14 +5,19 @@ from src.main.python.lonely_integer.LonelyInteger import lonelyinteger
 
 class Test(TestCase):
     def test_lonelyinteger(self):
-        input = [1, 2, 3, 4, 3, 2, 1]
-        output = lonelyinteger(input)
+        input_data = [1, 2, 3, 4, 3, 2, 1]
+        output = lonelyinteger(input_data)
 
         self.assertEqual(4, output)
 
-
     def test_lonelyinteger_ii(self):
-            input = [0, 0, 1, 2, 1, 5, 5]
-            output = lonelyinteger(input)
+        input_data = [0, 0, 1, 2, 1, 5, 5]
+        output = lonelyinteger(input_data)
 
-            self.assertEqual(2, output)
+        self.assertEqual(2, output)
+
+    def test_lonelyinteger_single_number_list(self):
+        input_data = [0]
+        output = lonelyinteger(input_data)
+
+        self.assertEqual(0, output)
